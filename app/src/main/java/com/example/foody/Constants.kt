48e7d2bd2c9246695706cbe63709bc0bcbc38f23
Sak <jacubsak@gmail.com>
2024-01-29 22:09:41 +0100

    structure of models and fragments Added Interface for for API GET calls and class for storing constants for api calls

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.foody
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "c4as65fc43a455afc7436723abb7f1da163412a"
+
+    }
+
+}
